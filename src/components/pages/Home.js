00192b66a0d6ec5c5a75e2db4666e361cb7553c7
hsnadeeb <hsnadeeb007@gmail.com>
2023-11-06 01:40:31 +0530

    implemented the edit and delete function as well

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Form, Button, Container, Row, Col, Table, Dropdown } from 'react-bootstrap';
+import { Form, Button, Container, Row, Col, Table, Dropdown, Modal } from 'react-bootstrap';
 
 const Expenses = () => {
   const [moneySpent, setMoneySpent] = useState('');
   const [description, setDescription] = useState('');
   const [category, setCategory] = useState('Select');
   const [expenses, setExpenses] = useState([]);
+  const [selectedExpense, setSelectedExpense] = useState(null);
+  const [showEditModal, setShowEditModal] = useState(false);
 
   const fetchExpenseData = () => {
     fetch("https://authenticate-8c62d-default-rtdb.firebaseio.com/expenses.json")
@@ -17,10 +19,9 @@ const Expenses = () => {
         }
       })
       .then((data) => {
-        // Handle the data you received from the database and update expenses state
         const expenseData = [];
         for (const key in data) {
-          expenseData.push(data[key]);
+          expenseData.push({ id: key, ...data[key] });
         }
         setExpenses(expenseData);
       })
@@ -30,7 +31,6 @@ const Expenses = () => {
   };
 
   useEffect(() => {
-    // Fetch and populate user data when the component mounts
     fetchExpenseData();
   }, []);
 
@@ -57,21 +57,60 @@ const Expenses = () => {
       })
       .then((data) => {
         console.log("Data submitted successfully:", data);
+        setMoneySpent('');
+        setDescription('');
+        setCategory('Food');
+        fetchExpenseData();
       })
       .catch((error) => {
         console.error("Error:", error);
       });
-
-    setExpenses([...expenses, newExpense]);
-    setMoneySpent('');
-    setDescription('');
-    setCategory('Food');
   };
 
-  const removeExpense = (index) => {
-    const updatedExpenses = [...expenses];
-    updatedExpenses.splice(index, 1);
-    setExpenses(updatedExpenses);
+  const removeExpense = (id) => {
+    fetch(`https://authenticate-8c62d-default-rtdb.firebaseio.com/expenses/${id}.json`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("Expense deleted successfully");
+          fetchExpenseData();
+        } else {
+          throw new Error("Failed to delete expense");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  const openEditModal = (expense) => {
+    setSelectedExpense(expense);
+    setShowEditModal(true);
+  };
+
+  const saveEditedExpense = () => {
+    if (selectedExpense) {
+      fetch(`https://authenticate-8c62d-default-rtdb.firebaseio.com/expenses/${selectedExpense.id}.json`, {
+        method: "PUT",
+        body: JSON.stringify(selectedExpense),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+            console.log("Expense edited successfully");
+            setShowEditModal(false);
+            fetchExpenseData();
+          } else {
+            throw new Error("Failed to edit expense");
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
   };
 
   return (
@@ -111,7 +150,6 @@ const Expenses = () => {
                   <Dropdown.Item onClick={() => setCategory('Food')}>Food</Dropdown.Item>
                   <Dropdown.Item onClick={() => setCategory('Petrol')}>Petrol</Dropdown.Item>
                   <Dropdown.Item onClick={() => setCategory('Salary')}>Salary</Dropdown.Item>
-                  {/* Add more categories as needed */}
                 </Dropdown.Menu>
               </Dropdown>
             </Form.Group>
@@ -137,14 +175,17 @@ const Expenses = () => {
               </tr>
             </thead>
             <tbody>
-              {expenses.map((expense, index) => (
-                <tr key={index}>
+              {expenses.map((expense) => (
+                <tr key={expense.id}>
                   <td>{expense.moneySpent}</td>
                   <td>{expense.description}</td>
                   <td>{expense.category}</td>
                   <td>
-                    <Button variant="danger" size="sm" onClick={() => removeExpense(index)}>
+                    <Button variant="danger" size="sm" onClick={() => removeExpense(expense.id)}>
                       Delete
+                    </Button>
+                    <Button variant="info" size="sm" onClick={() => openEditModal(expense)}>
+                      Edit
                     </Button>
                   </td>
                 </tr>
@@ -153,6 +194,53 @@ const Expenses = () => {
           </Table>
         </Col>
       </Row>
+
+      <Modal show={showEditModal} onHide={() => setShowEditModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Expense</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group>
+              <Form.Label>Money Spent</Form.Label>
+              <Form.Control
+                type="number"
+                value={selectedExpense ? selectedExpense.moneySpent : ''}
+                onChange={(e) => setSelectedExpense({ ...selectedExpense, moneySpent: e.target.value })}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                type="text"
+                value={selectedExpense ? selectedExpense.description : ''}
+                onChange={(e) => setSelectedExpense({ ...selectedExpense, description: e.target.value })}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Category</Form.Label>
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  {selectedExpense ? selectedExpense.category : ''}
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item onClick={() => setSelectedExpense({ ...selectedExpense, category: 'Food' })}>Food</Dropdown.Item>
+                  <Dropdown.Item onClick={() => setSelectedExpense({ ...selectedExpense, category: 'Petrol' })}>Petrol</Dropdown.Item>
+                  <Dropdown.Item onClick={() => setSelectedExpense({ ...selectedExpense, category: 'Salary' })}>Salary</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowEditModal(false)}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={saveEditedExpense}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 };
